a3f6b8f31654ee0b72b4de9b0d9fe80da94e569c
Cassandra Keddis <cassandra@thinkific.com>
2023-03-06 21:11:54 -0800

    exercise6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Alexis", last_name: "Curran", hourly_rate: 60)
+@store1.employees.create(first_name: "Jessica", last_name: "Lake", hourly_rate: 60)
+@store1.employees.create(first_name: "Jason", last_name: "Meijers", hourly_rate: 60)
+@store1.employees.create(first_name: "Tara", last_name: "Cain", hourly_rate: 60)
+@store2.employees.create(first_name: "Brandy", last_name: "Burton", hourly_rate: 60)
+@store2.employees.create(first_name: "Kirsteen", last_name: "McJannet", hourly_rate: 60)
+@store2.employees.create(first_name: "Laura", last_name: "Bin", hourly_rate: 60)
+@store2.employees.create(first_name: "Ryan", last_name: "Vanderbrink", hourly_rate: 60)
